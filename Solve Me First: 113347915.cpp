4736ad8c6072971fc9523013a618619394d850b2
Firoz Ahmed Hazari <35864518+Riyuzaaki@users.noreply.github.com>
2023-09-09 22:03:46 +0530

    Add files via upload

---
+++ Solve Me First: 113347915.cpp
@@ -0,0 +1,46 @@
+//https://www.hackerrank.com/challenges/solve-me-first/copy-from/113347915
+
+/*
+Complete the function solveMeFirst to compute the sum of two integers.
+
+Example
+
+
+Return .
+
+Function Description
+
+Complete the solveMeFirst function in the editor below.
+
+solveMeFirst has the following parameters:
+
+int a: the first value
+int b: the second value
+Returns
+- int: the sum of  and 
+
+Constraints
+
+
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+ // Hint: Type return a+b; below:
+  
+}
+
+int main() {
+  int num1, num2;
+  int sum;
+  cin>>num1>>num2;
+  sum = solveMeFirst(num1,num2);
+  cout<<sum;
+  return 0;
+}
